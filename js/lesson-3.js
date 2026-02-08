207e432c04c790f76b06b784e299b300c643d190
Oleksandr_Kobzar <oleksandrkobzar@gmail.com>
2026-02-08 19:33:46 +0200

    Update

---
+++ js/lesson-3.js
@@ -0,0 +1,217 @@
+'use strict';
+//!=========================================
+// Завдання 1:
+// Дано масив чисел [1, 2, 3, 4, 5]. Створіть новий масив, що містить
+// квадрати кожного елементу вхідного масиву.
+// Очікуваний результат: [1, 4, 9, 16, 25].
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// const newNumber = arr => arr.map(n => n ** 2);
+// console.log(newNumber(numbers));
+
+// const newNumber = numbers.map(n => n ** 2);
+// console.log(newNumber);
+
+//!=========================================
+// Завдання 2:
+// Дано масив об'єктів. Створіть новий масив, що містить всі значення
+// з масивів values кожного об'єкту, збережених в одному масиві.
+// Очікуваний результат: [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+// const data = [
+//   { id: 1, values: [1, 2, 3] },
+//   { id: 2, values: [4, 5, 6] },
+//   { id: 3, values: [7, 8, 9] },
+// ];
+
+// const flattenedValues = data.flatMap(d => d.values);
+// console.log(flattenedValues);
+
+//!=========================================
+// Завдання 3:
+// Дано масив об'єктів.
+// Перевірте, чи є хоча б один об'єкт з віком менше 20 років.
+// Очікуваний результат: true.
+
+// const peoples = [
+//   { name: 'John', age: 27 },
+//   { name: 'Jane', age: 31 },
+//   { name: 'Bob', age: 19 },
+// ];
+
+// const ageUnderTwenty = peoples.some(people => people.age < 20);
+// console.log(ageUnderTwenty);
+
+//!=========================================
+// Завдання 4:
+// Дано масив чисел [2, 4, 6, 8, 10].
+// Перевірте, чи є кожен елемент масиву парним. Очікуваний результат: true.
+
+// const numbers = [2, 4, 6, 8, 10];
+
+// const pairedElement = numbers.every(number => number % 2 === 0);
+// console.log(pairedElement);
+
+//!=========================================
+// Завдання 5:
+// Знайдіть перше непарне число
+
+// const numbers = [2, 1, 6, 8, 9, 10, 12];
+
+// const firstNonpairedElement = numbers.find(number => number % 2 !== 0);
+// console.log(firstNonpairedElement);
+
+//!=========================================
+// Завдання 6:
+// Відсортуйте масив чисел [4, 2, 5, 1, 3]
+// у порядку зростання. Очікуваний результат: [1, 2, 3, 4, 5].
+
+// const numbersArray = [4, 2, 5, 1, 3];
+
+// const sortElement = numbersArray.toSorted((a, b) => a - b);
+// console.log(sortElement);
+
+//!=========================================
+// Завдання 7:
+// Відсортуйте масив рядків ["banana", "orange", "apple", "pear"]
+//  у порядку алфавіту.
+// Очікуваний результат: ["apple", "banana", "orange", "pear"].
+
+// const stringArray = ['banana', 'orange', 'apple', 'pear'];
+
+// const sortAlphabet = stringArray.toSorted();
+// console.log(sortAlphabet);
+
+// const sortAlphabet1 = stringArray.toSorted((a, b) => a.localeCompare(b));
+// console.log(sortAlphabet1);
+
+//!=========================================
+// Завдання 8:
+//  Відсортуйте масив об'єктів за віком у порядку зростання.
+// Очікуваний результат: [{name: "Bob", age: 19}, {name: "John", age: 27}, {name: "Jane", age: 31}].
+
+// const users = [
+//   { name: 'John', age: 27 },
+//   { name: 'Jane', age: 31 },
+//   { name: 'Bob', age: 19 },
+// ];
+
+// const sortUsers = users.toSorted((a, b) => a.age - b.age);
+// console.log(sortUsers);
+
+//!=========================================
+// Завдання 9:
+// Дано масив об'єктів.
+// Створіть новий масив, що містить тільки об'єкти, в яких
+// вік більше 20 років.
+// Очікуваний результат: [{name: "John", age: 27}, {name: "Jane", age: 31}]
+
+// const user = [
+//   { name: 'John', age: 27 },
+//   { name: 'Jane', age: 31 },
+//   { name: 'Bob', age: 19 },
+// ];
+
+// const filterUsers = user.filter(u => u.age > 20);
+// console.log(filterUsers);
+
+//!=========================================
+// Завдання 10:
+// Дано масив чисел [1, 2, 3, 4, 5].
+// Застосуйте метод для обчислення суми елементів масиву.
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// const sumOfElem = numbers.reduce((previousValue, number) => previousValue + number, 0);
+// console.log(sumOfElem);
+
+//!=========================================
+// Завдання 11:
+// Розроби клас Calculator, який дозволяє виконувати арифметичні
+// операції над числом за допомогою методів класу, підтримуючи
+// ланцюжковий виклик (method chaining).
+//
+// Вимоги до класу Calculator
+// - Метод number(value)
+// Встановлює початкове значення для наступних обчислень.
+// Повертає сам об'єкт (this) для підтримки ланцюжкових викликів.
+
+// - метод getResult, Повертає поточний результат усіх операцій.
+// Не змінює значення, просто повертає його.
+
+// - метод add - Додає value до поточного значення.
+// Повертає сам об'єкт (this) для підтримки ланцюжкових викликів.
+
+// - метод subtract - Віднімає value від поточного значення. Повертає this.
+
+// - метод divide - Ділить поточне значення на value, якщо value не дорівнює 0.
+// Якщо value === 0, викидає помилку з повідомленням про неможливість ділення.
+// Повертає сам об'єкт (this) для підтримки ланцюжкових викликів.
+
+// - метод multiply -Множить поточне значення на value. Повертає this.
+
+// Об'єкт класу може проводити послідовні операції у вигляді ланцюжка
+
+// Приклад використання:
+
+class Calculator {
+  number(value) {
+    this.value = value;
+    return this;
+  }
+
+  add(value) {
+    this.value += value;
+    return this;
+  }
+
+  subtract(value) {
+    this.value -= value;
+    return this;
+  }
+
+  multiply(value) {
+    this.value *= value;
+    return this;
+  }
+
+  divide(value) {
+    if (value === 0) {
+      alert('Ділити на 0 не можна!');
+    } else {
+      this.value /= value;
+    }
+    return this;
+  }
+
+  getResult() {
+    return this.value;
+  }
+
+  //   get value() {}
+  //   set value(newValue) {}
+}
+
+const calc = new Calculator();
+console.log(calc);
+
+const result = calc
+  .number(10) // Встановлюємо початкове значення 10
+  .add(5) // Додаємо 5 (10 + 5 = 15)
+  .subtract(3) // Віднімаємо 3 (15 - 3 = 12)
+  .multiply(4) // Множимо на 4 (12 * 4 = 48)
+  .divide(2) // Ділимо на 2 (48 / 2 = 24)
+  .getResult(); // Отримуємо результат: 24
+
+console.log(result); // 24
+
+// calc.value = 50;
+// console.log(calc.value);
+//!=========================================
+// Завдання 12:
+// Напиши клас Client який створює об'єкт з властивостями login email.
+// Оголоси приватні властивості #login #email, доступ до яких зроби
+// через геттер та сеттер login email
+
+class Client {}
